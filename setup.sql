394029337bf4d47b44b03b69d783bb2eac740827
Igor Krasovskiy <39698272+mykras@users.noreply.github.com>
2022-08-26 10:46:14 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,4 +1,11 @@
-CREATE TABLE nv (
-  devtodev_ID INT PRIMARY KEY,
-  title TEXT
-);
+CREATE TABLE test_jinni (
+  candidate_id INT NOT NULL,
+  hire_reported TIMESTAMP NOT NULL,
+  primary_keyword_candidate VARCHAR(45) NOT NULL,
+  candidates_city VARCHAR(45) NULL,
+  candidates_country VARCHAR(45) NULL,
+  experience_years INT NOT NULL,
+  en_level_candidate VARCHAR(45) NULL,
+  hire_salary INT NOT NULL,
+  company_type VARCHAR(45) NOT NULL,
+  PRIMARY KEY (candidate_id));
