3c355c562c9dd640555664db544f1de1c056fff8
Igor Krasovskiy <39698272+mykras@users.noreply.github.com>
2022-08-26 19:24:24 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,11 +1,11 @@
 CREATE TABLE jinni (
-  candidate_id INT PRIMARY KEY,
-  hire_reported TEXT,
-  primary_keyword_candidate TEXT,
-  candidates_city TEXT,
-  candidates_country TEXT,
-  experience_years INT,
-  en_level_candidate TEXT,
-  hire_salary INT,
-  company_type TEXT
-  );
+  candidate_id INT NOT NULL,
+  hire_reported TIMESTAMP NOT NULL,
+  primary_keyword_candidate VARCHAR(45) NOT NULL,
+  candidates_city VARCHAR(45) NULL,
+  candidates_country VARCHAR(45) NULL,
+  experience_years INT NOT NULL,
+  en_level_candidate VARCHAR(45) NULL,
+  hire_salary INT NOT NULL,
+  company_type VARCHAR(45) NOT NULL,
+  PRIMARY KEY (candidate_id));
