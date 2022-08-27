1f95e20d46d35e6c9d2c080adf93582d71f799d1
Igor Krasovskiy <39698272+mykras@users.noreply.github.com>
2022-08-27 20:30:32 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,11 +1,24 @@
+drop table jinni;
 CREATE TABLE jinni (
-  candidate_id INT PRIMARY KEY,
+  candidate_id INT,
   hire_reported TEXT,
   primary_keyword_candidate TEXT,
   candidates_city TEXT,
   candidates_country TEXT,
-  experience_years INT,
+  experience_years FLOAT,
   en_level_candidate TEXT,
-  hire_salary INT,
+  hire_salary FLOAT,
   company_type TEXT
   );
+COPY jinni(
+  candidate_id,
+  hire_reported,
+  primary_keyword_candidate,
+  candidates_city,
+  candidates_country,
+  experience_years,
+  en_level_candidate,
+  hire_salary,
+  company_type
+  ) FROM '/Users/Shared/test_jinni.csv'
+DELIMITER ',' CSV HEADER;
